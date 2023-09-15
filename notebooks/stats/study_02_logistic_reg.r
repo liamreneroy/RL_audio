95a88c4ce10c7ba3f311a25ac1cf1e0d295ad53d
Liam Roy <liamreneroy@gmail.com>
2023-09-15 16:09:05 +1000

    before second logi regression

--- notebooks/stats/study_02_logistic_reg.r
+++ notebooks/stats/study_02_logistic_reg.r
@@ -10,6 +10,11 @@
 # Reference Code (StatQuest)
 # https://github.com/StatQuest/logistic_regression_demo/blob/master/logistic_regression_demo.R
 
+# References for Explaining Output
+# https://quantifyinghealth.com/logistic-regression-in-r-with-categorical-variables/
+# https://towardsdatascience.com/simply-explained-logistic-regression-with-example-in-r-b919acb1d6b3
+# https://stats.idre.ucla.edu/r/dae/logit-regression/
+
 ### INSTALLING PACKAGES ###
 ## if you see the version is out of date, run: update.packages()
 
@@ -26,48 +31,76 @@ library("cowplot")
 ### CHECK CURRENT DIRECTORY LOCATION
 getwd() 
 
-# Load in Data for State 0, Section 2U (Uninformed)
-st0_sect2U_data <- read_excel("notebooks/user_data/response_book.xlsx", sheet="regression_st0_sect2U", range = "A1:G649", col_names = TRUE, col_types = NULL, na = "", skip = 0)
+# Load in Data for State 0/1/2, Section 2U/2I (All datasets combined -> 27 cubes x 24 users = 648 samples ... 648 x 6 = 3888 samples)
+regression_data <- read_excel("notebooks/user_data/response_book.xlsx", sheet="regression_combined", range = "A1:I3889", col_names = TRUE, col_types = NULL, na = "", skip = 0)
 
 # Look at the data
-head(st0_sect2U_data)
+head(regression_data)
 
 # Remove the first and third column (Value and User ID)
-st0_sect2U_data_trim <- subset(st0_sect2U_data, select = -c(1, 3))
+regression_data_trim <- subset(regression_data, select = -c(1, 3))
 
 # Look at the data structure (see what type of data is in each column)
-str(st0_sect2U_data_trim)
+str(regression_data_trim)
 
 # Currently our acoustic parameters are type 'num', we need to modify this
 # Note: The next lines convert ordinal data to factors (catagorical)
 # Note, we don't convert the confidence to a factor, since it is a continuous variable (0.0-10.0)
-st0_sect2U_data_trim$P1_BPM <- as.factor(st0_sect2U_data_trim$P1_BPM)
-st0_sect2U_data_trim$P2_BPL <- as.factor(st0_sect2U_data_trim$P2_BPL)
-st0_sect2U_data_trim$P3_Pitch <- as.factor(st0_sect2U_data_trim$P3_Pitch)
+regression_data_trim$P1_BPM <- as.character(regression_data_trim$P1_BPM)
+regression_data_trim[regression_data_trim$P1_BPM == '0',]$P1_BPM <- "=0"
+regression_data_trim[regression_data_trim$P1_BPM == '1',]$P1_BPM <- "=1"
+regression_data_trim[regression_data_trim$P1_BPM == '2',]$P1_BPM <- "=2"
+regression_data_trim$P1_BPM <- as.factor(regression_data_trim$P1_BPM)
+
+
+regression_data_trim$P2_BPL <- as.character(regression_data_trim$P2_BPL)
+regression_data_trim[regression_data_trim$P2_BPL == '0',]$P2_BPL <- "=0"
+regression_data_trim[regression_data_trim$P2_BPL == '1',]$P2_BPL <- "=1"
+regression_data_trim[regression_data_trim$P2_BPL == '2',]$P2_BPL <- "=2"
+regression_data_trim$P2_BPL <- as.factor(regression_data_trim$P2_BPL)
+
+regression_data_trim$P3_Pitch <- as.character(regression_data_trim$P3_Pitch)
+regression_data_trim[regression_data_trim$P3_Pitch == '0',]$P3_Pitch <- "=0"
+regression_data_trim[regression_data_trim$P3_Pitch == '1',]$P3_Pitch <- "=1"
+regression_data_trim[regression_data_trim$P3_Pitch == '2',]$P3_Pitch <- "=2"
+regression_data_trim$P3_Pitch <- as.factor(regression_data_trim$P3_Pitch)
+
+# Change States 0 / 1 / 2 to factors (catagorical)
+regression_data_trim$State <- as.character(regression_data_trim$State)
+regression_data_trim[regression_data_trim$State == '0',]$State <- "=Stuck"
+regression_data_trim[regression_data_trim$State == '1',]$State <- "=Accom."
+regression_data_trim[regression_data_trim$State == '2',]$State <- "=Progr."
+regression_data_trim$State <- as.factor(regression_data_trim$State)
+
+# Change Init U / I to factors (catagorical)
+regression_data_trim[regression_data_trim$Init == "U",]$Init <- "=Uninformed"
+regression_data_trim[regression_data_trim$Init == "I",]$Init <- "=Informed"
+regression_data_trim$Init <- as.factor(regression_data_trim$Init)
 
 # Replace the binary output for 'Correct' with "Correct" and "Incorrect"
-st0_sect2U_data_trim$Correct <- ifelse(test=st0_sect2U_data_trim$Correct == 1, yes="Correct", no="Incorrect")
-st0_sect2U_data_trim$Correct <- as.factor(st0_sect2U_data_trim$Correct) # Now convert to a factor
+regression_data_trim$Correct <- ifelse(test=regression_data_trim$Correct == 1, yes="Correct", no="Incorrect")
+regression_data_trim$Correct <- as.factor(regression_data_trim$Correct) # Now convert to a factor
 
 # Look at the data structure again (see what type of data is in each column)
-str(st0_sect2U_data_trim)
+str(regression_data_trim)
 
 # Good practice: check that there is a good amount of samples for correct and incorrect responses
-table(st0_sect2U_data_trim$Correct)
-
+table(regression_data_trim$Correct)
 
 # Good practice: exclude variables that only have 1 or 2 samples in a category
 # since +/- one or two samples can have a large effect on the odds/log(odds)
 # We only need to do this for our data that is catagorical (not continuous)
-xtabs(~ Correct + P1_BPM, data=st0_sect2U_data_trim)
-xtabs(~ Correct + P2_BPL, data=st0_sect2U_data_trim)
-xtabs(~ Correct + P3_Pitch, data=st0_sect2U_data_trim)
+xtabs(~ Correct + P1_BPM, data=regression_data_trim)
+xtabs(~ Correct + P2_BPL, data=regression_data_trim)
+xtabs(~ Correct + P3_Pitch, data=regression_data_trim)
+xtabs(~ Correct + State, data=regression_data_trim)
+xtabs(~ Correct + Init, data=regression_data_trim)
 
 
 # Now lets run a logistic regression using the glm function
 # Lets look at the model with all variables
-st0_sect2U_logit <- glm(Correct ~ P1_BPM + P2_BPL + P3_Pitch + Confidence, data=st0_sect2U_data_trim, family='binomial')
-summary(st0_sect2U_logit)
+regression_logit <- glm(Correct ~ P1_BPM + P2_BPL + P3_Pitch + Confidence + State + Init, data=regression_data_trim, family='binomial')
+summary(regression_logit)
 
 ## QUICK DIVE INTO RESULTS
 # BPM and BPL do not show a significant effect (p > 0.05)
@@ -93,8 +126,8 @@ summary(st0_sect2U_logit)
 #
 # The null model is a model with only an intercept (no predictors)
  
-ll.null <- st0_sect2U_logit$null.deviance/-2  # Null model
-ll.proposed <- st0_sect2U_logit$deviance/-2   # Model w/ all parameters
+ll.null <- regression_logit$null.deviance/-2  # Null model
+ll.proposed <- regression_logit$deviance/-2   # Model w/ all parameters
  
 
 # McFadden's Pseudo R^2 = [ LL(Null) - LL(Proposed) ] / LL(Null)
@@ -105,31 +138,31 @@ ll.proposed <- st0_sect2U_logit$deviance/-2   # Model w/ all parameters
 
 # Chi-square value = 2*(LL(Proposed) - LL(Null))
 # p-value = 1 - pchisq(chi-square value, df = 2-1)
-1 - pchisq(2*(ll.proposed - ll.null), df=(length(st0_sect2U_logit$coefficients)-1))
+1 - pchisq(2*(ll.proposed - ll.null), df=(length(regression_logit$coefficients)-1))
 
 # Note that the resulting p-value is so tiny it is essentially zero (p < 0.0001)
 
 
 # Now plot the data:
 # Create a new dataframe that contains the predicted probabilities for each sample
-predicted.st0_sect2U_logit <- data.frame(
-  probability.of.Correct=st0_sect2U_logit$fitted.values,
-  Correct=st0_sect2U_data_trim$Correct)
+predicted.regression_logit <- data.frame(
+  probability.of.Correct=regression_logit$fitted.values,
+  Correct=regression_data_trim$Correct)
 
 # Sort the data by the predicted probability
-predicted.st0_sect2U_logit <- predicted.st0_sect2U_logit[
-  order(predicted.st0_sect2U_logit$probability.of.Correct, decreasing=FALSE),]
+predicted.regression_logit <- predicted.regression_logit[
+  order(predicted.regression_logit$probability.of.Correct, decreasing=FALSE),]
 
 # Add a column that contains the index of each sample
-predicted.st0_sect2U_logit$rank <- 1:nrow(predicted.st0_sect2U_logit)
+predicted.regression_logit$rank <- 1:nrow(predicted.regression_logit)
 
 # Lastly, plot the predicted probabilities for each sample and colour each sample based on
 # whether that sample is actually labeled as 'correct' or 'incorrect' 
-ggplot(data=predicted.st0_sect2U_logit, aes(x=rank, y=probability.of.Correct)) +
+ggplot(data=predicted.regression_logit, aes(x=rank, y=probability.of.Correct)) +
   geom_point(aes(color=Correct), alpha=1, shape=4, stroke=2) +
   xlab("Data Point Index") +
   ylab("Predicted Probability of Correctly Identified Robot State\n") +
-    ggtitle("Logistic Regression for State 0, Section 2U\n") +
+    ggtitle("Logistic Regression for Correctly Identified Robot States\n") +
     theme(plot.title = element_text(hjust = 0.5)) +
     scale_color_manual(values=c("#40a100", "#ff6600")) +
     theme(legend.title=element_blank()) +
