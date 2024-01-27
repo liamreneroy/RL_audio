eac83b342e3f077116faa27c600e35e85e6fe5de
Liam Roy <liamreneroy@gmail.com>
2024-01-27 11:54:50 +1100

    cleaned repo for public

--- notebooks/stats/locating_profile.r
+++
@@ -1,9 +0,0 @@
-candidates <- c( Sys.getenv("R_PROFILE"),
-             file.path(Sys.getenv("R_HOME"), "etc", "Rprofile.site"),
-             Sys.getenv("R_PROFILE_USER"),
-             file.path(getwd(), ".Rprofile"),
-             file.path(Sys.getenv("HOME"), ".Rprofile"))
-
-Filter(file.exists, candidates)
-
-candidates
